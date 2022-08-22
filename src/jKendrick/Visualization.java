753b99add7c2f526ad3e99193fc16d2912c24048
Yvan Guifo <56076081+YvanGuifo@users.noreply.github.com>
2022-08-22 09:44:43 +0100

    Change the legend of class visualization of SIR Model

--- src/jKendrick/Visualization.java
+++ src/jKendrick/Visualization.java
@@ -33,7 +33,7 @@ public class Visualization {
 		}
 				
 	// Create Chart
-	final XYChart chart = new XYChartBuilder().width(600).height(400).theme(ChartTheme.XChart).title("SIR Model").xAxisTitle("Time (year)").yAxisTitle("Number of individuals").build();
+	final XYChart chart = new XYChartBuilder().width(600).height(400).theme(ChartTheme.XChart).title("SIR Model").xAxisTitle("Time (days)").yAxisTitle("Proportion of individuals").build();
 
 	// Customize Chart
 		chart.getStyler().setLegendPosition(LegendPosition.InsideNE);
