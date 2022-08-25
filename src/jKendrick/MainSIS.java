d292b7369e28947c7e1a633b1956446496d2b60b
Yvan Guifo <56076081+YvanGuifo@users.noreply.github.com>
2022-08-25 06:28:25 +0100

    Refactor the code. Add information of xAxis, yAxis and title of main class to SIS model

--- src/jKendrick/MainSIS.java
+++ src/jKendrick/MainSIS.java
@@ -13,13 +13,16 @@ public class MainSIS {
 		double last = 70.;
 		
 		String[] seriesNames = {"S", "I"};
+		String title = "SIS Model";
+		String xAxis = "Time (days)";
+		String yAxis = "Proportion of individuals";
 		
 		double[] arguments ={ 0.999999, 0.000001};
 		double [][] results = 	integratorExample(step, last, arguments);
 		Visualization viz = new Visualization();
 		
 		try {
-			viz.xchartExample(results, step, last, seriesNames);
+			viz.xchartExample(results, step, last, seriesNames, title, xAxis, yAxis);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
