95eb474c5e2507183596279518bd25ea5853c1db
sophiedepierre <sophiedepierre@stagemovey.lip6.fr>
2023-06-02 11:49:59 +0200

    Tensor sum start

---
+++ src/jKendrick/tools/TensorSum.java
@@ -0,0 +1,15 @@
+package jKendrick.tools;
+
+public class TensorSum {
+	
+	public double[][][][] getTensorSum(double[][] a,double[][] b){
+		int xA=a.length;
+		int yA=a[0].length;
+		int xB=b.length;
+		int yB=b[0].length;
+		double[][][][] s=new double[xA][yA][xB][yB];
+		//todo
+		return s;
+	}
+
+}
