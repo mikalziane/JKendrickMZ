e2a1f64946cecb49166452ef30bb9f79917dfd87
sophiedepierre <sophiedepierre@stagemovey.lip6.fr>
2023-06-02 15:37:08 +0200

    Tensor sum test

--- src/jKendrick/tools/TensorSum.java
+++ src/jKendrick/tools/TensorSum.java
@@ -2,13 +2,13 @@ package jKendrick.tools;
 
 public class TensorSum {
 	
-	public double[][][][] getTensorSum(double[][] a,double[][] b){
+	public static double[][][][] getTensorSum(double[][] a,double[][] b){
 		int xA=a.length;
 		int yA=a[0].length;
 		int xB=b.length;
 		int yB=b[0].length;
+		assert xA==yA && xB==yB;
 		double[][][][] s=new double[xA][yA][xB][yB];
-		//todo
 		double[][] iA=getIdentity(a);
 		double[][] iB=getIdentity(b);
 		double[][][][] pA=tensorProduct(a,iB);
@@ -25,12 +25,13 @@ public class TensorSum {
 		return s;
 	}
 	
-	public double[][] getIdentity(double[][] m){
+	public static double[][] getIdentity(double[][] m){
 		int x=m.length;
 		int y=m[0].length;
+		assert x==y;
 		double[][] identity=new double[x][y];
 		for(int i=0; i<x;++i) {
-			for(int j=0;j<y;++i) {
+			for(int j=0;j<y;++j) {
 				if(i==j) {
 					identity[i][j]=1.;
 				}
@@ -42,11 +43,12 @@ public class TensorSum {
 		return identity;
 	}
 	
-	public double[][][][] tensorProduct(double[][] a, double[][] b){
+	public static double[][][][] tensorProduct(double[][] a, double[][] b){
 		int xA=a.length;
 		int yA=a[0].length;
 		int xB=b.length;
 		int yB=b[0].length;
+		assert xA==yA && xB==yB;
 		double[][][][] tP=new double[xA][yA][xB][yB];
 		for(int i=0;i<xA;++i) {
 			for(int j=0;j<yA;++j) {
