1167d1dd6b18b377a5d9f39bcf41a5ec51069449
sophiedepierre <sophiedepierre@stagemovey.lip6.fr>
2023-06-07 17:02:06 +0200

    Composition start

---
+++ src/jKendrick/scenario/Scenario.java
@@ -0,0 +1,17 @@
+package jKendrick.scenario;
+
+import java.util.List;
+import java.util.Map;
+
+import jKendrick.concerns.Concern;
+
+public class Scenario {
+	List<Concern> concerns;
+	
+	
+	public Scenario(List<Concern> concerns) {
+		this.concerns=concerns;
+		
+	}
+
+}
