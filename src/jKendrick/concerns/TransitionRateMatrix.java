e38d3f1a13e14175f6449261ff77d6e4da8eb887
Nemerrelysse <115214198+Nemerrelysse@users.noreply.github.com>
2023-06-12 16:45:55 +0200

    Transition Rates Matrix

---
+++ src/jKendrick/concerns/TransitionRateMatrix.java
@@ -0,0 +1,54 @@
+package jKendrick.concerns;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TransitionRateMatrix {
+	private HashMap<XY,Rate> rates;
+	private ArrayList<String> compartments;
+	
+	public TransitionRateMatrix(ArrayList<String> compartments) {
+		rates=new HashMap<XY,Rate>();
+		this.compartments=compartments;
+	}
+	
+	public void setRate(String x, String y, Rate rate) {
+		assert (compartments.contains(x) && compartments.contains(y));
+		XY xy=new XY(x,y);
+		rates.put(xy,rate);
+	}
+	
+	public void addCompartment(String c) {
+		if(!compartments.contains(c)) {
+			compartments.add(c);
+		}
+	}
+	
+	public double getRate(String x, String y) {
+		assert (compartments.contains(x) && compartments.contains(y));
+		double rate=0.;
+		XY xy=new XY(x,y);
+		if(x.equals(y)) {
+			for(int i=0;i<compartments.size();++i) {
+				if(i!=compartments.indexOf(x)) {
+					rate+=getRate(x,compartments.get(i));
+				}
+			}
+		}
+		else if (rates.containsKey(xy)) {
+			rate=rates.get(xy).getValue();
+		}
+		return rate;
+	}
+	
+	
+	private static class XY{
+		private String x;
+		private String y;
+		public XY(String x, String y) {
+			this.x=x;
+			this.y=y;
+		}
+	}
+
+}
